a542172556cb5c3be973724ab17a6cc82efe938c
Quvonchbek <fendygamer2006@gmail.com>
2023-11-18 11:55:45 +0500

    exam commited

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    static {
+        try {
+
+            FileHandler fileHandler = new FileHandler("output/logs",true);
+            SimpleFormatter simpleFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(simpleFormatter);
+            log.addHandler(fileHandler);
+
+        } catch (IOException e) {
+
+            log.warning(e.getMessage());
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
